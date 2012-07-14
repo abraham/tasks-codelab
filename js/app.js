925ea13a150a12c0406c3e10e508c60cc0ffd5d8
abraham <4braham@gmail.com>
2012-07-14 16:01:13 -0700

    added function to add auth headers to api request

--- js/app.js
+++ js/app.js
@@ -23,3 +23,18 @@ var Account = Backbone.Model.extend({
     // this.on('change:access_token', this.fetch);
   }
 });
+
+/***
+  * Attach authentication headers to API requests.
+  */
+function attacheAuthHeader(xhr, settings) {
+  if (!window.account) {
+    return;
+  }
+  if (settings.url.indexOf('https://www.googleapis.com/') === 0 && window.account.get('access_token')) {
+    xhr.setRequestHeader('Authorization', 'Bearer ' + window.account.get('access_token'));
+  }
+}
+$.ajaxSetup({
+  beforeSend: attacheAuthHeader
+});
